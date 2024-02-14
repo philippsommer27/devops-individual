6ae41ecafb1b74c549abe17b4798b1726d347ab2
philippsommer27 <philippsommerhalter@gmail.com>
2024-02-14 11:35:08 +0100

    basic setup

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,3 +1,5 @@
+from swagger_server.service.student_service import *
+
 import connexion
 import six
 
@@ -17,7 +19,8 @@ def add_student(body=None):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, 'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +33,10 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+        return delete(body)
+    return 500, 'error'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +49,7 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+        return get_by_id(body)
+    return 500, 'error'
